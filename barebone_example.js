fa7b09b43262a812b4c8196c09dd4aa9169eae42
mariarose <mariarosemcguire@gmail.com>
2016-04-15 21:47:43 +0000

    second commit with Terri

--- barebone_example.js
+++ barebone_example.js
@@ -1,22 +1,63 @@
-if (Meteor.isClient) {
+//if (Meteor.isClient) {
 
-Template.inputFromUser.events({
-  'submit .new-comment'(event) {
+//Template.inputFromUser.events({
+  //'submit .new-comment'(event) {
     // Prevent default browser form submit
-    event.preventDefault();
+   // event.preventDefault();
 
     // Get value from form element
-    const target = event.target;
-    const text = target.text.value;
+   // const target = event.target;
+   // const text = target.text.value;
 
     // Insert a task into the collection
-    commentsAboutBernie.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    //commentsAboutBernie.insert({
+     // text,
+     // createdAt: new Date(), // current time
+   // });
 
     // Clear form
-    target.text.value = '';
-  },
-});
+   // target.text.value = '';
+//  },
+//});
+//}
+
+if (Meteor.isClient) {
+  Template.inputFromUser.events({
+    'submit .new-task'(event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      const target = event.target;
+      const textValue = target.text.value;
+
+      console.log("The value of target is...", target);
+      console.log("The value of textValue is...", textValue);
+
+      // Insert a task into the collection
+      Tasks.insert({
+        textValue,
+        createdAt: new Date(), // current time
+      });
+
+      // Clear form
+      target.text.value = '';
+    },
+  });
+
+  var commentOutputHelperDictionary = {profile:
+                                     {
+                                      comment:"Hey! How are you",
+                                      name:{first:"yo", last:"duude"}
+                                     }
+                                    };
+
+Template.inputFromOthers.helpers(commentOutputHelperDictionary);
+
+  if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
 }
